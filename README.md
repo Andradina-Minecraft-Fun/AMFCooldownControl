4d0b8ff8534a44f68c73a5fb515c7aacc223d7fe
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2021-01-24 22:03:32 -0300

    Added config file and example how to hook custom tag

--- README.md
+++ README.md
@@ -10,6 +10,66 @@ To control that, we remove minecraft attack speed and do our attack speed system
 
 Watch on Youtube: [![Watch on Youtube](https://img.youtube.com/vi/Ed3oYDNuxpA/maxresdefault.jpg)](https://youtu.be/Ed3oYDNuxpA)
 
+# Hook your plugin
+
+To create a plugin who can give attack speed, just hook `AMFCooldownControl` and add the tag that you add on config.yml
+
+For example:
+
+```
+// ...
+
+@Override
+public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+{   
+    // Get command  
+    String commandName = cmd.getName().toLowerCase();
+
+    // Get AMFCooldownControl plugin
+    Plugin AMFCooldownControl = Bukkit.getServer().getPluginManager().getPlugin("AMFCooldownControl");
+    
+    // If is my command (/test <attack_speed_integer_value>)
+    if(commandName.equals("test")) {
+    
+        // Get item on hand and meta
+        ItemStack item = player.getEquipment().getItemInMainHand();
+        ItemMeta meta = item.getItemMeta();
+    
+        // Add lore
+        ArrayList<String> lore = new ArrayList<String>();
+        lore.add("Attack Speed " + args[0]);
+        meta.setLore(lore);
+    
+        // Add tag with name added on config.yml
+        NamespacedKey key = new NamespacedKey(AMFCooldownControl, "AMFAttackSpeed");
+        meta.getPersistentDataContainer().set(key, PersistentDataType.DOUBLE, Double.parseDouble(args[0]));
+    
+        // Return meta to item
+        item.setItemMeta(meta);
+    }
+    
+    return true;
+}
+// ...
+```
+
+# AMFCooldownControl config.yml
+
+You can use the AMFCooldownControl without external plugin if you just want remove cooldown hits, and can ignore custom tag name. All the fields of config documentated are:
+
+```
+# If true, will show msg to player: time to next hit, weapon attack speed bonus, alert when cooldown finish and alert 
+#     that hit cannot done. This will appear for every player hit
+debug: false
+
+# Default base attack speed. Without modifiers, the player will hit every baseAttackSpeed seconds
+baseAttackSpeed: 1.4
+
+# Name of custom tag name. If you will add custom tag to give attack speed bonus, this is the name tag to be added.
+# Don't forget that custom tags are plugin related, so you need to add custom tag to AMFCooldownControl plugin
+CustomTagName: AMFAttackSpeed
+
+```
 
 
 
--- src/main/java/br/com/centralandradina/amfcooldowncontrol/AMFCooldownControl.java
+++ src/main/java/br/com/centralandradina/amfcooldowncontrol/AMFCooldownControl.java
@@ -2,15 +2,24 @@ package br.com.centralandradina.amfcooldowncontrol;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.inventory.meta.tags.ItemTagType;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public final class AMFCooldownControl extends JavaPlugin implements Listener
@@ -18,11 +27,23 @@ public final class AMFCooldownControl extends JavaPlugin implements Listener
     // Armazena o player que está em cooldown
     static HashSet<Player> cooldownAttackList = new HashSet<>();
 
+    // Armazena a configuração
+    public FileConfiguration config;
+
     /**
      * On load do plugin
      */
     @Override
     public void onEnable() {
+
+        // Config
+        config = this.getConfig();
+        config.addDefault("debug", false);
+        config.addDefault("baseAttackSpeed", 1.4);
+        config.addDefault("CustomTagName", "AMFAttackSpeed");
+        config.options().copyDefaults(true);
+        saveConfig();
+
         // Plugin startup logic
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
 
@@ -71,7 +92,7 @@ public final class AMFCooldownControl extends JavaPlugin implements Listener
             if (cooldownAttackList.contains(attacker)) {
 
                 // Se o atacante está na lista de espera, cancela o hit
-                attacker.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cWait for next hit!"));
+                _debug(attacker,"&cWait for next hit!");
                 event.setCancelled(true);
             }
             else {
@@ -79,24 +100,62 @@ public final class AMFCooldownControl extends JavaPlugin implements Listener
                 // Se o atacante não está na lista de cooldown, adiciona ele à lista, e procede com o ataque normalmente
                 cooldownAttackList.add(attacker);
 
-                // @todo Calcular o attack speedy em segundos, baseado nos atributos do jogador, e bonus da arma
-                long delay = 1;
+                // Recupera o attack speed base
+                double baseAttackSpeed = config.getDouble("baseAttackSpeed");
+
+                // Verifica se o item de ataque tem a custom tag
+                ItemStack item = attacker.getEquipment().getItemInMainHand();
+                double weaponAttackSpeed = getAttackSpeedBonusFromItem(item);
+                if(weaponAttackSpeed > 0) {
+                    // baseAttackSpeed é a velocidade padrão. Então se a arma tem 2 de velocidade, é 2% a menos do baseAttackSpeed: baseAttackSpeed - ((baseAttackSpeed * weaponAttackSpeed) / 100)
+                    baseAttackSpeed = (baseAttackSpeed - ((baseAttackSpeed * weaponAttackSpeed) / 100));
+                    _debug(attacker,"&4Has weapon attack speed " + weaponAttackSpeed);
+                }
+
+                // @todo Verificar algum item particular no inventário (estilo totem)
+                //      Adicionar no config se é utiliza ou nao este metodo para nao fazer processamento a toa
+
+                // @todo Verificar algum item de armadura possui attack speed
+                //      Adicionar no config se é utiliza ou nao este metodo para nao fazer processamento a toa
 
                 // Timer para remover o atacante da lista de cooldown
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        attacker.sendMessage(ChatColor.translateAlternateColorCodes('&', "&dCooldown finish"));
+                        _debug(attacker,"&dCooldown finish");
                         cooldownAttackList.remove(attacker);
                     }
-                }.runTaskLater(this, delay * 20);
-
+                }.runTaskLater(this, (long)(baseAttackSpeed * 20.0));
+                _debug(attacker,"&7Next hit on " + baseAttackSpeed + "s (" + (long)(baseAttackSpeed * 20.0) + " ticks)");
             }
         }
     }
 
+    /**
+     * Recupera o bonus do item
+     */
+    public double getAttackSpeedBonusFromItem(ItemStack item)
+    {
+        String customTagName = config.getString("CustomTagName");
+        ItemMeta itemMeta = item.getItemMeta();
+
+        NamespacedKey key = new NamespacedKey(this, customTagName);
+        PersistentDataContainer tagContainer = itemMeta.getPersistentDataContainer();
+
+        return tagContainer.getOrDefault(key, PersistentDataType.DOUBLE, 0.0);
+    }
+
+    /**
+     * Metodos só pra debug
+     */
     public String _color(String msg)
     {
         return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+    public void _debug(Player p, String msg)
+    {
+        if(config.getBoolean("debug")) {
+            p.sendMessage(_color(msg));
+        }
     }
 }
